15e8c6dfbc723af9782606042cea47010b85e6c6
Khurshed <khurshed-shavkatzhonovich@yandex.ru>
2025-04-15 22:52:25 +0500

    some changes

--- bot_zudrason/bot_zudrason/settings.py
+++ bot_zudrason/bot_zudrason/settings.py
@@ -85,9 +85,12 @@ WSGI_APPLICATION = 'bot_zudrason.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True  # Важно для Render!
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
